f7fd5d7ff2a8963f3ee7ca46f760814cf2eb424f
Artyom Shutoff <37169190+artyomshutoff@users.noreply.github.com>
2023-03-02 17:15:07 +0300

    Add files via upload

---
+++ Лаба 3/Сушков/working_file.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def f(x: np.ndarray) -> np.ndarray:
+    return (np.arctan(2 * x) * (x + 1)) / (x**3)
+
+
+x_min = 1  # Левая граница области
+x_max = 4  # Правая граница области
+
+# х - вектор 100 значений от x_min до x_max
+x = np.linspace(x_min, x_max, 100)
+y = f(x)  # вектор-функция от х
+
+y_min = min(y)  # Нижняя граница области
+y_max = max(y)  # Верхняя граница области
+
+# Рисуем график
+plt.plot(x, y)
+plt.axhline(y_max, color="red", linestyle="--")
+plt.axhline(y_min, color="red", linestyle="--")
+plt.axvline(x_min, color="red", linestyle="--")
+plt.axvline(x_max, color="red", linestyle="--")
+
+plt.show()
+
+# Количество точек в прямоугольной области, чтобы с вероятностью
+# р = 0.9 результат имел точность не менее 0.01
+N = 6806
+
+# Вектор полученных значений в испытаниях
+results = []
+
+# Площадь области испытаний - красного прямоугольника
+D_measure = (x_max - x_min) * (y_max - y_min)
+
+for _ in range(10):
+    # Генерируем случайные точки внутри области
+    exp_x = np.random.uniform(x_min, x_max, N)
+    exp_y = np.random.uniform(y_min, y_max, N)
+
+    # Считаем количество точек, попавших под
+    # или на график функции
+    m = len(exp_y[exp_y <= f(exp_x)])
+
+    results.append(round(D_measure * m / N, 4))
+
+
+print(*results, end="\n\n")
+print("Итоговое значение:", round(np.mean(results), 4))
